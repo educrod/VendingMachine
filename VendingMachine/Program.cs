25e97bef3af7376a53dd30c0fd302947c1aa0994
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-05-31 22:36:14 -0300

    Add project files.

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+            Console.WriteLine(vendingMachine.Dispense(2.00M));
+            
+        }
+    }
+}
